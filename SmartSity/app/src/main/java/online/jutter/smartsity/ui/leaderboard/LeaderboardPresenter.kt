80f385cd75426714dae10110523ff2250200e019
Yuri <51887884+JerzyXD@users.noreply.github.com>
2022-07-15 09:44:55 +0300

    Доделал таблицу победителей

--- SmartSity/app/src/main/java/online/jutter/smartsity/ui/leaderboard/LeaderboardPresenter.kt
+++ SmartSity/app/src/main/java/online/jutter/smartsity/ui/leaderboard/LeaderboardPresenter.kt
@@ -2,8 +2,12 @@ package online.jutter.smartsity.ui.leaderboard
 
 import com.arellomobile.mvp.InjectViewState
 import online.jutter.smartsity.Screens
+import online.jutter.smartsity.data.net.models.CompetitionResponse
+import online.jutter.smartsity.data.net.models.LeaderboardResponse
 import online.jutter.smartsity.data.net.models.MemberResponse
+import online.jutter.smartsity.domain.controllers.BottomVisibilityController
 import online.jutter.smartsity.domain.usecases.GetLeaderBoardUserCase
+import online.jutter.smartsity.ui.leaderboard.info.LeaderboardInfoView
 import online.jutter.supersld.common.base.BasePresenter
 import online.jutter.supersld.extensions.launchIO
 import online.jutter.supersld.extensions.withUI
@@ -13,6 +17,7 @@ import org.koin.core.inject
 class LeaderboardPresenter: BasePresenter<LeaderboardView>() {
 
     private val getScheduleListUseCase: GetLeaderBoardUserCase by inject()
+    private val bottomVisibilityController: BottomVisibilityController by inject()
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
@@ -20,8 +25,13 @@ class LeaderboardPresenter: BasePresenter<LeaderboardView>() {
         loadSchedule()
     }
 
-    fun leaderboardInfo(members: MutableList<MemberResponse>) {
-        router?.navigateTo(Screens.LeaderboardInfo(members))
+    fun leaderboardInfo(leaderboard: LeaderboardResponse) {
+        router?.navigateTo(Screens.LeaderboardInfo(leaderboard))
+    }
+
+    override fun attachView(view: LeaderboardView?) {
+        super.attachView(view)
+        bottomVisibilityController.show()
     }
 
     fun loadSchedule() {
--- SmartSity/app/src/main/java/online/jutter/smartsity/ui/leaderboard/info/LeaderBoardInfoAdapter.kt
+++ SmartSity/app/src/main/java/online/jutter/smartsity/ui/leaderboard/info/LeaderBoardInfoAdapter.kt
@@ -1,19 +1,44 @@
 package online.jutter.smartsity.ui.leaderboard.info
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_member.view.*
+import online.jutter.smartsity.R
+import online.jutter.smartsity.data.net.models.MemberResponse
 
-class LeaderBoardInfoAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class LeaderBoardInfoAdapter(
+    private val memberList: MutableList<MemberResponse> = mutableListOf()
+): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        val v: View = LayoutInflater.from(parent.context).inflate(R.layout.item_member, parent, false)
+        return ItemViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        memberList[position].let { (holder as LeaderBoardInfoAdapter.ItemViewHolder).bind(it) }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return memberList.size
+    }
+
+    fun addAll(newItems: MutableList<MemberResponse>) {
+        memberList.clear()
+        memberList.addAll(newItems)
+        notifyDataSetChanged()
+    }
+
+    inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(item: MemberResponse) {
+            with(itemView) {
+                tvPlace.text = "#${item.rank}"
+                tvName.text = item.name
+            }
+        }
+
     }
 }
