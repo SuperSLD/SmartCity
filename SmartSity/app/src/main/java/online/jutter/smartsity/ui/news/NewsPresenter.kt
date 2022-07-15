35b2581a0368b660c76a3769e024a417875c47a5
Dmitry Sviridovsky <di.sviridovskiy@gmail.com>
2022-07-15 07:34:24 +0300

    фикс

--- SmartSity/app/src/main/java/online/jutter/smartsity/ui/news/NewsPresenter.kt
+++ SmartSity/app/src/main/java/online/jutter/smartsity/ui/news/NewsPresenter.kt
@@ -23,10 +23,15 @@ class NewsPresenter: BasePresenter<NewsView>() {
     private val bottomVisibilityController: BottomVisibilityController by inject()
     private val getNewsUseCase: GetNewsUseCase by inject()
 
+    override fun attachView(view: NewsView?) {
+        super.attachView(view)
+
+        bottomVisibilityController.show()
+    }
+
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
 
-        bottomVisibilityController.show()
         loadList()
     }
 
